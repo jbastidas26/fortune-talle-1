2bf405c76a891e58c9773b9fa9ef050c1a057d1e
jbastidas26 <43036752+jbastidas26@users.noreply.github.com>
2019-04-22 16:44:16 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+      var text;
+
+$(".btn").click(function(){
+    text = $(".ipt").val();
+    console.log(text);
+    $(".target").html(text);
+});
+    var text2;
+
+$(".btn").click(function(){
+    text2 = $(".ipt2").val();
+    console.log(text2);
+    $(".target2").html(text2);
+    
+    
+    
+});
+ var text3;
+
+$(".btn").click(function(){
+    text3 = $(".ipt3").val();
+    console.log(text3);
+    $(".target3").html(text3);
+    
+    
+    
+});
